f1a6ca403d9ef9ba99fc3f83ce48abc15ec58018
Raif Atef <beliskner.github.3psil0N@neverbox.com>
2018-11-09 01:57:52 +0200

    Apply workaround for CommandParameter binding: https://stackoverflow.com/a/24669638/111830

---
+++ src/MonBand.Windows/UI/Helpers/ButtonHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace MonBand.Windows.UI.Helpers
+{
+    public static class ButtonHelper
+    {
+        public static readonly DependencyProperty CommandParameterProperty = DependencyProperty.RegisterAttached(
+            "CommandParameter",
+            typeof(object),
+            typeof(ButtonHelper),
+            new PropertyMetadata(CommandParameterChanged));
+
+        static void CommandParameterChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var target = d as ButtonBase;
+            if (target == null)
+                return;
+
+            target.CommandParameter = e.NewValue;
+            var temp = target.Command;
+
+            // Have to set it to null first or CanExecute won't be called.
+            target.Command = null;
+            target.Command = temp;
+        }
+
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        public static object GetCommandParameter(ButtonBase target)
+        {
+            return target.GetValue(CommandParameterProperty);
+        }
+
+        [SuppressMessage("ReSharper", "UnusedMember.Global")]
+        public static void SetCommandParameter(ButtonBase target, object value)
+        {
+            target.SetValue(CommandParameterProperty, value);
+        }
+    }
+}
