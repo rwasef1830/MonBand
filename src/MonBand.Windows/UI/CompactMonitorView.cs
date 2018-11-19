9556c4cd15b7a68237b08055d20fa7105f03bd72
Raif Atef <beliskner.github.3psil0N@neverbox.com>
2018-11-19 20:31:06 +0200

    Refactor out CompactMonitorView and cleanup DeskbandControl.

---
+++ src/MonBand.Windows/UI/CompactMonitorView.cs
@@ -0,0 +1,123 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using MonBand.Core;
+using MonBand.Windows.Models;
+using OxyPlot;
+using OxyPlot.Wpf;
+
+namespace MonBand.Windows.UI
+{
+    public class CompactMonitorView : UserControl
+    {
+        public static readonly DependencyProperty MonitorNameProperty = DependencyProperty.Register(
+            nameof(MonitorName),
+            typeof(string),
+            typeof(CompactMonitorView));
+
+        readonly BandwidthPlotModel _plotModel;
+        TextBlock _textBlockMaximum;
+        TextBlock _textBlockDownloadBandwidth;
+        TextBlock _textBlockUploadBandwidth;
+
+        public string MonitorName
+        {
+            get => (string)this.GetValue(MonitorNameProperty);
+            set => this.SetValue(MonitorNameProperty, value);
+        }
+
+        public CompactMonitorView()
+        {
+            this._plotModel = new BandwidthPlotModel(100)
+            {
+                PlotMargins = new OxyThickness(0),
+                Padding = new OxyThickness(0),
+                IsLegendVisible = false,
+                BandwidthAxis = { IsAxisVisible = false }
+            };
+
+            this.Content = this.CreateControls();
+        }
+
+        Grid CreateControls()
+        {
+            var monitorNameTextBlock = new TextBlock();
+            monitorNameTextBlock.SetBinding(
+                TextBlock.TextProperty,
+                new Binding(nameof(this.MonitorName)) { Source = this });
+
+            this._textBlockMaximum = new TextBlock { Text = "Maximum: 0.0 Mbps" };
+
+            this._textBlockDownloadBandwidth = new TextBlock
+            {
+                FontSize = 10,
+                Foreground = Brushes.Blue,
+                Text = "D: -",
+                Margin = new Thickness(1, 1, 2, 0)
+            };
+            this._textBlockUploadBandwidth = new TextBlock
+            {
+                FontSize = 10,
+                Foreground = Brushes.Red,
+                Text = "U: -",
+                Margin = new Thickness(1, 0, 2, 2)
+            };
+
+            var rootGrid = new Grid
+            {
+                ColumnDefinitions =
+                {
+                    { new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) } }
+                },
+                RowDefinitions =
+                {
+                    { new RowDefinition { Height = new GridLength(1, GridUnitType.Star) } }
+                },
+                ToolTip = new Label
+                {
+                    Content = new StackPanel
+                    {
+                        Orientation = Orientation.Vertical,
+                        Children =
+                        {
+                            monitorNameTextBlock,
+                            this._textBlockMaximum
+                        }
+                    }
+                },
+                Children =
+                {
+                    new PlotView { Model = this._plotModel },
+                    new Label
+                    {
+                        Padding = new Thickness(1),
+                        Content = new StackPanel
+                        {
+                            Orientation = Orientation.Vertical,
+                            Background = Brushes.White,
+                            Children =
+                            {
+                                this._textBlockDownloadBandwidth,
+                                this._textBlockUploadBandwidth
+                            }
+                        }
+                    }
+                }
+            };
+
+
+            return rootGrid;
+        }
+
+        public void AddTraffic(NetworkTraffic traffic)
+        {
+            var megabits = traffic.AsMegabits();
+            this._plotModel.AddTraffic(megabits.InMegabits, megabits.OutMegabits);
+            this._textBlockDownloadBandwidth.Text = $"D: {megabits.InMegabits:F1} Mbps";
+            this._textBlockUploadBandwidth.Text = $"U: {megabits.OutMegabits:F1} Mbps";
+            this._plotModel.InvalidatePlot(true);
+            this._textBlockMaximum.Text = $"Maximum: {this._plotModel.BandwidthAxis.DataMaximum:F1} Mbps";
+        }
+    }
+}
--- src/MonBand.Windows/UI/DeskbandControl.xaml.cs
+++ src/MonBand.Windows/UI/DeskbandControl.xaml.cs
@@ -1,28 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 using Microsoft.Extensions.Logging.Abstractions;
 using MonBand.Core;
 using MonBand.Core.Snmp;
 using MonBand.Core.Util;
-using MonBand.Windows.Models;
 using MonBand.Windows.Settings;
-using OxyPlot;
-using OxyPlot.Wpf;
 
 namespace MonBand.Windows.UI
 {
     public partial class DeskbandControl
     {
-        readonly IDictionary<
-            ITrafficRateService,
-            (BandwidthPlotModel Model,
-            TextBlock DownTextBlock,
-            TextBlock UpTextBlock,
-            TextBlock MaximumTextBlock)> _objectsByService;
+        readonly IDictionary<ITrafficRateService, CompactMonitorView> _viewsByService;
 
         public DeskbandControl() : this(AppSettings.Load()) { }
 
@@ -30,8 +20,7 @@ namespace MonBand.Windows.UI
         {
             if (settings == null) throw new ArgumentNullException(nameof(settings));
 
-            this._objectsByService =
-                new Dictionary<ITrafficRateService, (BandwidthPlotModel, TextBlock, TextBlock, TextBlock)>();
+            this._viewsByService = new Dictionary<ITrafficRateService, CompactMonitorView>();
 
             this.InitializeComponent();
             this.InitializeMonitors(settings.SnmpPollers);
@@ -54,76 +43,13 @@ namespace MonBand.Windows.UI
                     SystemTimeProvider.Instance,
                     new NullLoggerFactory());
 
-                var maximumTextBlock = new TextBlock { Text = "Maximum: 0.0 Mbps" };
-
-                var container = new Grid
-                {
-                    ColumnDefinitions = { new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) } },
-                    RowDefinitions = { new RowDefinition { Height = new GridLength(1, GridUnitType.Star) } },
-                    ToolTip = new Label
-                    {
-                        Content = new StackPanel
-                        {
-                            Orientation = Orientation.Vertical,
-                            Children =
-                            {
-                                new TextBlock { Text = snmpPoller.ToString() },
-                                maximumTextBlock
-                            }
-                        }
-                    }
-                };
-
-                var plotModel = new BandwidthPlotModel(100)
-                {
-                    PlotMargins = new OxyThickness(0),
-                    Padding = new OxyThickness(0),
-                    IsLegendVisible = false,
-                    BandwidthAxis = { IsAxisVisible = false }
-                };
                 trafficRateService.TrafficRateUpdated += this.HandleTrafficRateUpdated;
 
-                var plot = new PlotView { Model = plotModel };
-                container.Children.Add(plot);
+                var view = new CompactMonitorView { MonitorName = snmpPoller.ToString() };
+                this._viewsByService[trafficRateService] = view;
 
-                var textBlockStackPanel = new StackPanel
-                {
-                    Orientation = Orientation.Vertical,
-                    Background = Brushes.White
-                };
-                var downTextBlock = new TextBlock
-                {
-                    FontSize = 10,
-                    Foreground = Brushes.Blue,
-                    Margin = new Thickness(0, 0, 2, 0),
-                    Text = "D: -"
-                };
-                var upTextBlock = new TextBlock
-                {
-                    FontSize = 10,
-                    Foreground = Brushes.Red,
-                    Margin = new Thickness(0, 0, 2, 0),
-                    Text = "U: -"
-                };
-                textBlockStackPanel.Children.Add(downTextBlock);
-                textBlockStackPanel.Children.Add(upTextBlock);
-
-                var currentBandwidthLabel = new Label { Content = textBlockStackPanel, Padding = new Thickness(3) };
-                container.Children.Add(currentBandwidthLabel);
-
-                this.RootGrid.ColumnDefinitions.Add(
-                    new ColumnDefinition
-                    {
-                        Width = new GridLength(1, GridUnitType.Star)
-                    });
-                Grid.SetColumn(container, i);
-                this.RootGrid.Children.Add(container);
-
-                this._objectsByService[trafficRateService] = (
-                    plotModel,
-                    downTextBlock,
-                    upTextBlock,
-                    maximumTextBlock);
+                Grid.SetColumn(view, i);
+                this.RootGrid.Children.Add(view);
 
                 trafficRateService.Start();
             }
@@ -131,18 +57,8 @@ namespace MonBand.Windows.UI
 
         void HandleTrafficRateUpdated(object sender, NetworkTraffic traffic)
         {
-            var objects = this._objectsByService[(ITrafficRateService)sender];
-
-            var megabits = traffic.AsMegabits();
-            objects.Model.AddTraffic(megabits.InMegabits, megabits.OutMegabits);
-
-            this.Dispatcher.Invoke(() =>
-            {
-                objects.DownTextBlock.Text = $"D: {megabits.InMegabits:F1} Mbps";
-                objects.UpTextBlock.Text = $"U: {megabits.OutMegabits:F1} Mbps";
-                objects.Model.InvalidatePlot(true);
-                objects.MaximumTextBlock.Text = $"Maximum: {objects.Model.BandwidthAxis.DataMaximum:F1} Mbps";
-            });
+            var view = this._viewsByService[(ITrafficRateService)sender];
+            this.Dispatcher.Invoke(() => view.AddTraffic(traffic));
         }
     }
 }
