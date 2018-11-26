89d76b68c09128d11ba7c6d4e1e7161166dfcf25
Raif Atef <beliskner.github.3psil0N@neverbox.com>
2018-11-26 17:37:24 +0200

    Refactor out PollingTrafficRateServiceBase from SnmpPollingTrafficRateService.

---
+++ src/MonBand.Core/PollingTrafficRateServiceBase.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace MonBand.Core
+{
+    public abstract class PollingTrafficRateServiceBase : ITrafficRateService
+    {
+        readonly TimeSpan _pollInterval;
+        readonly Func<TimeSpan, CancellationToken, Task> _delayTaskFactory;
+        readonly CancellationTokenSource _cancellationTokenSource;
+        readonly object _startStopLocker = new object();
+        bool _disposed;
+        Task _pollLoop;
+
+        protected ILogger Log { get; }
+
+        public event EventHandler<NetworkTraffic> TrafficRateUpdated;
+
+        protected PollingTrafficRateServiceBase(
+            byte pollIntervalSeconds,
+            ILoggerFactory loggerFactory,
+            Func<TimeSpan, CancellationToken, Task> delayTaskFactory)
+        {
+            this._pollInterval = TimeSpan.FromSeconds(pollIntervalSeconds);
+            this.Log = loggerFactory?.CreateLogger(this.GetType().Name)
+                        ?? throw new ArgumentNullException(nameof(loggerFactory));
+            this._delayTaskFactory = delayTaskFactory ?? throw new ArgumentNullException(nameof(delayTaskFactory));
+            this._cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        public void Start()
+        {
+            lock (this._startStopLocker)
+            {
+                if (this._disposed)
+                {
+                    throw new ObjectDisposedException(this.GetType().Name);
+                }
+
+                this._pollLoop = Task.Run(this.DoPollLoop);
+            }
+        }
+
+        async Task DoPollLoop()
+        {
+            var cancellationToken = this._cancellationTokenSource.Token;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    await this.PollAsync(cancellationToken).ConfigureAwait(false);
+                }
+                catch (OperationCanceledException)
+                {
+                    // ignore
+                }
+                catch (Exception ex)
+                {
+                    this.Log.LogError(ex, "Unhandled exception in poll loop.");
+                }
+                finally
+                {
+                    await this._delayTaskFactory(this._pollInterval, cancellationToken).ConfigureAwait(false);
+                }
+            }
+        }
+
+        protected abstract Task PollAsync(CancellationToken cancellationToken);
+
+        protected void OnTrafficRateUpdated(NetworkTraffic traffic)
+        {
+            this.TrafficRateUpdated?.Invoke(this, traffic);
+        }
+
+        public void Dispose()
+        {
+            lock (this._startStopLocker)
+            {
+                if (this._disposed)
+                {
+                    return;
+                }
+
+                this._cancellationTokenSource.Cancel();
+                try
+                {
+                    this._pollLoop.GetAwaiter().GetResult();
+                }
+                catch
+                {
+                    // ignore
+                }
+
+                this._pollLoop.Dispose();
+                this._cancellationTokenSource.Dispose();
+                this._disposed = true;
+            }
+        }
+    }
+}
--- src/MonBand.Core/Snmp/SnmpPollingTrafficRateService.cs
+++ src/MonBand.Core/Snmp/SnmpPollingTrafficRateService.cs
@@ -6,19 +6,12 @@ using MonBand.Core.Util;
 
 namespace MonBand.Core.Snmp
 {
-    public class SnmpPollingTrafficRateService : ITrafficRateService
+    public class SnmpPollingTrafficRateService : PollingTrafficRateServiceBase
     {
         readonly ISnmpTrafficQuery _trafficQuery;
-        readonly TimeSpan _pollInterval;
         readonly ITimeProvider _timeProvider;
-        readonly ILogger _log;
-        readonly Func<TimeSpan, CancellationToken, Task> _delayTaskFactory;
-        readonly CancellationTokenSource _cancellationTokenSource;
-        readonly object _startStopLocker = new object();
-        bool _disposed;
-        Task _pollLoop;
-
-        public event EventHandler<NetworkTraffic> TrafficRateUpdated;
+        DateTimeOffset _previousTime;
+        NetworkTraffic _previousTraffic;
 
         public SnmpPollingTrafficRateService(
             ISnmpTrafficQuery trafficQuery,
@@ -36,126 +29,61 @@ namespace MonBand.Core.Snmp
             byte pollIntervalSeconds,
             ITimeProvider timeProvider,
             ILoggerFactory loggerFactory,
-            Func<TimeSpan, CancellationToken, Task> delayTaskFactory)
+            Func<TimeSpan, CancellationToken, Task> delayTaskFactory) : base(
+            pollIntervalSeconds,
+            loggerFactory,
+            delayTaskFactory)
         {
             this._trafficQuery = trafficQuery
-                                        ?? throw new ArgumentNullException(nameof(trafficQuery));
-            this._pollInterval = TimeSpan.FromSeconds(pollIntervalSeconds);
+                                 ?? throw new ArgumentNullException(nameof(trafficQuery));
             this._timeProvider = timeProvider ?? throw new ArgumentNullException(nameof(timeProvider));
-            this._log = loggerFactory?.CreateLogger(this.GetType().Name)
-                        ?? throw new ArgumentNullException(nameof(loggerFactory));
-            this._delayTaskFactory = delayTaskFactory ?? throw new ArgumentNullException(nameof(delayTaskFactory));
-            this._cancellationTokenSource = new CancellationTokenSource();
         }
 
-        public void Start()
+        protected override async Task PollAsync(CancellationToken cancellationToken)
         {
-            lock (this._startStopLocker)
+            var traffic = await this._trafficQuery
+                .GetTotalTrafficBytesAsync(cancellationToken)
+                .ConfigureAwait(false);
+            this.Log.LogTrace(
+                "Traffic in bytes: Received: {0} - Sent: {1}",
+                traffic.InBytes,
+                traffic.OutBytes);
+
+            var now = this._timeProvider.UtcNow;
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (this._previousTime != default)
             {
-                if (this._disposed)
+                var secondsDelta = (now - this._previousTime).TotalSeconds;
+                var receivedBytesDelta = traffic.InBytes - this._previousTraffic.InBytes;
+                var sentBytesDelta = traffic.OutBytes - this._previousTraffic.OutBytes;
+
+                if (receivedBytesDelta < 0)
                 {
-                    throw new ObjectDisposedException(this.GetType().Name);
+                    // Counter wrap around occurred.
+                    receivedBytesDelta += uint.MaxValue;
                 }
 
-                this._pollLoop = Task.Run(this.DoPollLoop);
+                if (sentBytesDelta < 0)
+                {
+                    // Counter wrap around occurred.
+                    sentBytesDelta += uint.MaxValue;
+                }
+
+                var receivedBytesPerSecond = (long)(receivedBytesDelta / secondsDelta);
+                var sentBytesPerSecond = (long)(sentBytesDelta / secondsDelta);
+                var trafficRate = new NetworkTraffic(receivedBytesPerSecond, sentBytesPerSecond);
+
+                this.Log.LogDebug(
+                    "Traffic rate in bytes/sec: Received: {0} - Sent: {1}",
+                    trafficRate.InBytes,
+                    trafficRate.OutBytes);
+
+                this.OnTrafficRateUpdated(trafficRate);
             }
-        }
 
-        async Task DoPollLoop()
-        {
-            var cancellationToken = this._cancellationTokenSource.Token;
-
-            var previousTime = default(DateTimeOffset);
-            var previousTraffic = new NetworkTraffic(0, 0);
-
-            while (!cancellationToken.IsCancellationRequested)
-            {
-                try
-                {
-                    var traffic = await this._trafficQuery
-                        .GetTotalTrafficBytesAsync(cancellationToken)
-                        .ConfigureAwait(false);
-                    this._log.LogTrace(
-                        "Traffic in bytes: Received: {0} - Sent: {1}",
-                        traffic.InBytes,
-                        traffic.OutBytes);
-
-                    var now = this._timeProvider.UtcNow;
-                    cancellationToken.ThrowIfCancellationRequested();
-
-                    if (previousTime != default)
-                    {
-                        var secondsDelta = (now - previousTime).TotalSeconds;
-                        var receivedBytesDelta = traffic.InBytes - previousTraffic.InBytes;
-                        var sentBytesDelta = traffic.OutBytes - previousTraffic.OutBytes;
-
-                        if (receivedBytesDelta < 0)
-                        {
-                            // Counter wrap around occurred.
-                            receivedBytesDelta += uint.MaxValue;
-                        }
-
-                        if (sentBytesDelta < 0)
-                        {
-                            // Counter wrap around occurred.
-                            sentBytesDelta += uint.MaxValue;
-                        }
-
-                        var receivedBytesPerSecond = (long)(receivedBytesDelta / secondsDelta);
-                        var sentBytesPerSecond = (long)(sentBytesDelta / secondsDelta);
-                        var trafficRate = new NetworkTraffic(receivedBytesPerSecond, sentBytesPerSecond);
-
-                        this._log.LogDebug(
-                            "Traffic rate in bytes/sec: Received: {0} - Sent: {1}",
-                            trafficRate.InBytes,
-                            trafficRate.OutBytes);
-
-                        this.TrafficRateUpdated?.Invoke(
-                            this,
-                            trafficRate);
-                    }
-
-                    previousTime = now;
-                    previousTraffic = traffic;
-                }
-                catch (OperationCanceledException)
-                {
-                    // ignore
-                }
-                catch (Exception ex)
-                {
-                    this._log.LogError(ex, "Unhandled exception in poll loop.");
-                }
-                finally
-                {
-                    await this._delayTaskFactory(this._pollInterval, cancellationToken).ConfigureAwait(false);
-                }
-            }
-        }
-
-        public void Dispose()
-        {
-            lock (this._startStopLocker)
-            {
-                if (this._disposed)
-                {
-                    return;
-                }
-
-                this._cancellationTokenSource.Cancel();
-                try
-                {
-                    this._pollLoop.GetAwaiter().GetResult();
-                }
-                catch
-                {
-                    // ignore
-                }
-
-                this._pollLoop.Dispose();
-                this._cancellationTokenSource.Dispose();
-                this._disposed = true;
-            }
+            this._previousTime = now;
+            this._previousTraffic = traffic;
         }
     }
 }
